3c2216cefa56d5210d1dfadc4abdf6172b16adea
begumsnmz <92989034+begumsnmz@users.noreply.github.com>
2023-01-31 23:44:24 +0100

    Submitted versions *reupload*
    call_function - Task 14 version

--- Array/call_function.m
+++ Array/call_function.m
@@ -1,13 +1,16 @@
 initdata.d = 0.2; % m ; microphone distance
 initdata.c = 344; % m/s
 
-algo = DSB;
+% algo = DSB;
 % algo = DMA;
-% algo = DMA_GSLC;
+ algo = DMA_GSLC;
+% algo = dB_panning;
 
-sample = audioread('AIP_Song.wav');
+% sample = audioread('AIP_Song.wav');
 % sample = load('task11.mat').output;
+ sample = load('recording.mat').recording_buf;
 
 playdata = offline_sample_processing(sample, algo, initdata);
 
-sound(playdata(1:500000, :), 44100)
+% sound(playdata(1:500000, :), 44100)
+sound(playdata(:, :), 44100)
